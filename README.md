dd1af2f4578a8a0ccf47d940d1b8e6f3aeeb7959
theokrueger <theokrueger@airmail.cc>
2022-09-26 00:15:50 -0400

    new logo

--- README.md
+++ README.md
@@ -18,31 +18,6 @@ Afterwards enter the following command to build and install slock
 
     make clean install
 
-Dwmlogo patch
--------------
-This build contains the dwmlogo patch (look at the date to see which one is the latest), which instead of using the colors for the whole screen, it draws the dwm logo which changes color based on the state.
-
-### Logodwm in combination with other patches
-I have not tested the logodwm patch with all other patches, so if there is some incompatibility, make an issue or create a pull request.
-
-
-### Dwmlogo patch conflict with dpms patch
-With the `dpms` patch there is a conflict in the `main` function. Edit the `main` function so it looks like this, for it to work properly:
-
-    main(int argc, char **argv){
-        ...
-            XFreeGC(dpy, locks[s]->gc);
-        }
-
-        /* reset DPMS values to inital ones */
-        DPMSSetTimeouts(dpy, standby, suspend, off);
-        XSync(dpy, 0);
-        XCloseDisplay(dpy);
-
-        return 0;
-    }
-
-
 Running slock
 -------------
 Simply invoke the 'slock' command. To get out of it, enter your password.
--- config.def.h
+++ config.def.h
@@ -5,28 +5,51 @@ static const char *group = "nogroup";
 static const char *colorname[NUMCOLS] = {
 	[BACKGROUND] =   "black",     /* after initialization */
 	[INIT] =   "#2d2d2d",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
+	[INPUT] =  "#ff66cc",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
-/* insert grid pattern with scale 1:1, the size can be changed with logosize */
-static const int logosize = 75;
-static const int logow = 12;	/* grid width and height for right center alignment*/
-static const int logoh = 6;
+/* grid size and pos */
+static const int logosize = 25;
+static const int logow = 0;
+static const int logoh = 0;
 
-static XRectangle rectangles[9] = {
+static XRectangle rectangles[] = {
 	/* x	y	w	h */
-	{ 0,	3,	1,	3 },
-	{ 1,	3,	2,	1 },
-	{ 0,	5,	8,	1 },
-	{ 3,	0,	1,	5 },
-	{ 5,	3,	1,	2 },
-	{ 7,	3,	1,	2 },
-	{ 8,	3,	4,	1 },
-	{ 9,	4,	1,	2 },
-	{ 11,	4,	1,	2 },
-
+	/* smiley */
+	{ 1,	0,	1,	2 },
+	{ 3,	0,	1,	2 },
+	{ 0,	3,	1,	1 },
+	{ 1,	4,	3,	1 },
+	{ 4,	3,	1,	1 },
+	/* L */
+	{ 7,	0,	1,	5 },
+	{ 8,	4,	2,	1 },
+	/* O */
+	{ 11,	0,	3,	1 },
+	{ 11,	4,	3,	1 },
+	{ 11,	1,	1,	3 },
+	{ 13,	1,	1,	3 },
+	/* C */
+	{ 15,	0,	1,	5 },
+	{ 16,	0,	2,	1 },
+	{ 16,	4,	2,	1 },
+	/* K */
+	{ 19,	0,	1,	5 },
+	{ 20,	2,	1,	1 },
+	{ 21,	0,	1,	2 },
+	{ 21,	3,	1,	2 },
+	/* E */
+	{ 23,	0,	1,	5 },
+	{ 24,	0,	2,	1 },
+	{ 24,   2,      2,      1 },
+	{ 24,   4,      2,      1 },
+	/* D */
+	{ 27,	0,	1,	5 },
+	{ 28,	0,	1,	1 },
+	{ 28,	4,	1,	1 },
+	{ 29,	1,	1,	3 },
 };
